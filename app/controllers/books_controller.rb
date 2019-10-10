6988da8970fbc05dfbd5667f73d9d6f5064e5ea2
takuya yamaguchi <takuyayamaguchi@takuyanoMacBook-Pro.local>
2019-10-10 23:31:51 +0900

    Bookers change

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,36 +1,43 @@
 class BooksController < ApplicationController
 
   def index
-  	@books = Book.all
-  	@book = Book.new
-  	@book = Book.find(params[:id])
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
   end
 
   def edit
-  	@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to '/index'
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def update
-  	book = Book.find(params[:id])
+    book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  private
 
-  def book_params
-  	params.require(:book).permit(:title, :body)
+  def destroy
+     book = Book.find(params[:id])
+     book.destroy
+     redirect_to books_path
   end
+
+  private
+  def book_params
+      params.require(:book).permit(:title, :body)
+    end
+
 
 end
