e15cd4f75d3de01b05de890b3d0479dab113b787
takuya yamaguchi <takuyayamaguchi@takuyanoMacBook-Pro.local>
2019-10-11 23:33:03 +0900

    blog function added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,18 +19,21 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    flash[:notice] = 'Book was successfully created.'
+    redirect_to book_path(book.id)
   end
 
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:update] = 'Book was successfully updated.'
     redirect_to book_path(book.id)
   end
 
   def destroy
-     book = Book.find(params[:id])
-     book.destroy
+     @book = Book.find(params[:id])
+     @book.destroy
+     flash[:destroy] = 'Book was successfully destroyed.'
      redirect_to books_path
   end
 
